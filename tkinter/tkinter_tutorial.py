d85f3fc448d002104f3e248d72721d9561646def
vfwz <vfwz@gmail.com>
2018-06-30 00:29:55 +0800

    tkinter tutorial

---
+++ tkinter/tkinter_tutorial.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def click_me(btn, label):
+    btn.configure(text='Clicked')
+    btn.configure(state='disabled')
+    label.configure(foreground='red')
+
+
+def welcome(btn, label):
+    btn.configure(text='Welcomed')
+    # btn.configure(state='disabled')
+    label.configure(text='Welcome, ' + name.get() + '!')
+    label.configure(foreground='red')
+    label.grid(column=1, row=3)
+
+
+if __name__ == '__main__':
+    jm = tk.Tk()
+    jm.title("第一个图形界面")
+    jm.resizable(True, True)
+
+    row = 0
+    first_label = ttk.Label(jm, text="First Label")
+    first_label.grid(column=0, row=row)
+
+    row += 1
+    txt = ttk.Label(jm, text='Please Note The Label Color')
+    txt.grid(column=0, row=row)
+
+    click_btn = ttk.Button(jm, text='Click', command=lambda: click_me(btn=click_btn, label=txt))
+    click_btn.grid(column=1, row=row)
+
+    row += 1
+    name = tk.StringVar()
+    name_input = ttk.Entry(jm, width=12, textvariable=name)
+    name_input.grid(column=0, row=row)
+
+    welcome_label = ttk.Label(jm, text='')
+    welcome_label.grid_forget()
+
+    welcome_btn = ttk.Button(jm, text='Welcome', command=lambda: welcome(btn=welcome_btn, label=welcome_label))
+    welcome_btn.grid(column=1, row=row)
+
+    jm.mainloop()
